dea04e8ff6e3ee5355487a6be5e346640ca8cb8b
matheuscampos <matheusoliveira@inatel.br>
2017-08-17 11:21:50 -0300

    multi rooms

--- chatserver/src/server/WebServer.java
+++ chatserver/src/server/WebServer.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
@@ -16,12 +19,20 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/websocketendpoint", encoders = MessageEncoder.class, decoders = MessageDecoder.class)
 public class WebServer {
 	
-	static Set<Session> peers = Collections.synchronizedSet(new HashSet<>());
+	private static final String USERNAME_KEY = "username";
+	private static final String CHAT_CHANNEL = "channel";
+	private static Map<String, List<Session>> peers = Collections.synchronizedMap(new LinkedHashMap<String, List<Session>>());
 	
 	@OnOpen
 	public void onOpen(Session session){
-		System.out.println(session.getId() + " joined the chat room.");
-		peers.add(session);
+		
+		Map<String, List<String>> parameter = session.getRequestParameterMap();
+		List<String> username = parameter.get(USERNAME_KEY);
+		List<String> chatname = parameter.get(CHAT_CHANNEL);
+		List<Session> sessions =  peers.get(CHAT_CHANNEL);
+		sessions.add(session);
+		peers.put(chatname.get(0), sessions);
+		System.out.println(username + " joined the chat room.");
 	}
 	
 	@OnMessage
@@ -30,9 +41,8 @@ public class WebServer {
 		m.setSender(session.getId());
 		m.setContent(message);
 		m.setReceived(new Date());
-		for (Session peer : peers) {
+		for (Session peer : peers.values()) {
             if (!session.getId().equals(peer.getId())) { // do not resend the message to its sender
-            	
                 peer.getBasicRemote().sendObject(m);
             }
         }
@@ -43,7 +53,7 @@ public class WebServer {
 		System.out.println(session.getId()+" left the chat room.");
         peers.remove(session);
         //notify peers about leaving the chat room
-        for (Session peer : peers) {
+        for (Session peer : peers.values()) {
             Message message = new Message();
             message.setSender("Server");
             message.setContent((String) session.getUserProperties().get("user") + " left the chat room");
